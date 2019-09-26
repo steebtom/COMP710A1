8bb55d898d514cabda3d2b5c9d8b101b07063789
steebtom <steeb.webdev@gmail.com>
2019-09-26 13:04:30 +1200

    Views2

---
+++ resources/views/services.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.app')
+
+@section('content')
+<br>
+<br>
+
+<div class="container">
+<h2 class="aboutus-title">Cuts</h2>
+  <div class="row">
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+            <img src="/images/3.jpg" class="card-img-top" alt="...">
+            <div class="card-body " style="background-color:#B79CAB">
+                <h5 class="card-title">Crew Cut</h5>
+                <p class="card-text">Price: $25</p>
+                    <p class="card-text">Average Time: 40 mins</p>
+                    <p class="card-text">Stylist: Steeb</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+                <img src="/images/7.jpg" class="card-img-top" alt="...">
+                <div class="card-body " style="background-color:#B79CAB">
+                <h5 class="card-title">Mushroom Cut</h5>
+                <p class="card-text">Price: $50</p>
+                    <p class="card-text">Average Time: 10 mins</p>
+                    <p class="card-text">Stylist: Maria</p>
+                </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+                <img src="/images/6.jpg" class="card-img-top" alt="...">
+                <div class="card-body " style="background-color:#B79CAB">
+                <h5 class="card-title">Mullet</h5>
+                <p class="card-text">Price: $35</p>
+                    <p class="card-text">Average Time: 30 mins</p>
+                    <p class="card-text">Stylist: Steeb</p>
+                </div>
+        </div>
+    </div>
+  </div>
+  
+</div>
+
+<br>
+<br>
+
+<div class="container">
+<h2 class="aboutus-title">Products</h2>
+  <div class="row">
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+            <img src="/images/3.jpg" class="card-img-top" alt="...">
+            <div class="card-body " style="background-color:#FFEEDD">
+                <h5 class="card-title">Shampoo</h5>
+                <p class="card-text">Price: $25</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+                <img src="/images/7.jpg" class="card-img-top" alt="...">
+                <div class="card-body " style="background-color:#FFEEDD">
+                <h5 class="card-title">Conditioner</h5>
+                <p class="card-text">Price: $25</p>
+                </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+                <img src="/images/6.jpg" class="card-img-top" alt="...">
+                <div class="card-body " style="background-color:#FFEEDD">
+                <h5 class="card-title">Beard Oil</h5>
+                <p class="card-text">Price: $25</p>
+                </div>
+        </div>
+    </div>
+  </div>
+  
+</div>
+<br><br>
+<div class="container">
+<h2 class="aboutus-title">Other Services</h2>
+  <div class="row">
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+            <img src="/images/3.jpg" class="card-img-top" alt="...">
+            <div class="card-body " style="background-color:#FFCCD1">
+                <h5 class="card-title">Hair Coloring</h5>
+                <p class="card-text">Price: $25</p>
+                    <p class="card-text">Average Time: 40 mins</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+                <img src="/images/7.jpg" class="card-img-top" alt="...">
+                <div class="card-body " style="background-color:#FFCCD1">
+                <h5 class="card-title">Facial</h5>
+                <p class="card-text">Price: $50</p>
+                    <p class="card-text">Average Time: 10 mins</p>
+            
+                </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+                <img src="/images/6.jpg" class="card-img-top" alt="...">
+                <div class="card-body " style="background-color:#FFCCD1">
+                <h5 class="card-title">Pedicure</h5>
+                <p class="card-text">Price: $35</p>
+                    <p class="card-text">Average Time: 30 mins</p>
+                </div>
+        </div>
+    </div>
+  </div>
+  
+</div>
+
+@endsection
