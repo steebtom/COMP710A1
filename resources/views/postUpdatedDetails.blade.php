dde14966f9615e8c6271022f89a0b50f8ffc2e3c
steebtom <steeb.webdev@gmail.com>
2019-10-03 08:23:35 +1300

    Update details for admin

---
+++ resources/views/postUpdatedDetails.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+<?php
+
+$style = $_GET['style'];
+ $price = $_GET['price'];
+$id = $_GET['id'];
+
+
+?>
+
+<div class="card-body">
+                    <form method = "POST" action = "{{route('updateService')}}">
+                        @csrf
+
+<div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Cut Name') }}</label>
+
+                            <div class="col-md-2">  
+                            <input id="name" type="text" class="form-control" name="ser1" value = "<?php echo $style ?>"     autofocus>
+                            </div>
+                        </div>
+
+
+                        
+<div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
+                            <div class="col-md-2">
+                                <input id="name" type="text" class="form-control" name="ser2" value = "<?php echo $price ?>"  autofocus>
+                            </div>
+                        </div>
+
+                        <input id="id" type="hidden" class="form-control" name="ser3" value = "<?php echo $id ?>"  autofocus>
+                        
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class = "btn btn-primary" name = "submit">
+                                  Submit
+                                </button>
+
+                            
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+@endsection
