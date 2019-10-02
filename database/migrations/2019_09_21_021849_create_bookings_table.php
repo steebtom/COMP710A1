21f1ece162ac7c5faf7efc8d27eccca3012ee71c
MariaSusainathan <47832619+MariaSusainathan@users.noreply.github.com>
2019-10-02 13:48:22 +0530

    Updated Migrations
    Updated Migrations

---
+++ database/migrations/2019_09_21_021849_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->mediumText('name');
+            $table->string('email', 100);
+            $table->integer('phone');
+            $table->string('location', 100);
+            $table->date('bookingDate');
+            $table->string('slot', 100);
+            $table->string('stylist', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
